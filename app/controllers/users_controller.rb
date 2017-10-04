bde9f0a23b7fb3f1e5162a7a8195492482467259
onoro0313 <onoro.3836@gmail.com>
2017-10-04 17:27:49 +0900

    add edit update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+
+def edit
+end
+
+def update
+  binding.pry
+  user = current_user
+  current_user.update(update_params)
+  redirect_to root_path
+end
+
+private
+  def update_params
+    params.require(:user).permit(:name)
+  end
 end

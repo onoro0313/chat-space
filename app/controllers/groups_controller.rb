a5e5c0c578e4ca027cdb64f42a6e1def8e336f81
onoro0313 <onoro.3836@gmail.com>
2017-10-20 19:06:22 +0900

    group create/update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,18 +1,37 @@
 class GroupsController < ApplicationController
 
-  def index
-  end
+  # def index
+  # end
 
   def new
+    @group = Group.new
   end
 
   def create
+    @group = Group.new(group_params)
+      if @group.save
+        redirect_to root_path,notice: "グループの作成に成功しました"
+      else
+        render :new
+      end
+
   end
 
   def edit
+    @group = Group.find(params[:id])
   end
 
   def update
+    @group = Group.new(group_params)
+    if @group.update(group_params)
+      redirect_to root_path, notice: "グループを編集しました"
+    else
+      render :edit
+    end
   end
 
+  private
+  def group_params
+    params.require(:group).permit(:group_name, user_ids:[])
+  end
 end

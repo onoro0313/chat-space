61afe8debfd2680c599e13eef65b62925ad73309
onoro0313 <onoro.3836@gmail.com>
2017-10-20 19:06:08 +0900

    message save/validates

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
 devise_for :users
   root 'messages#index'
-  resources :groups, execpt: [:destroy,:show, ]
+  resources :groups, except: [:destroy,:show, ]
   resources :users, only: [:edit,:update]
 
 end

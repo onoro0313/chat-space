51b7e3ada3dfdc9f892ce3be9cc0d4742cf68cea
onoro0313 <onoro.3836@gmail.com>
2017-11-02 21:01:12 +0900

    create -> redirect_to index

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,17 +1,31 @@
 class MessagesController < ApplicationController
+# before_action :set_group, :set_messages
 
 def index
+  @group = Group.find(params[:group_id])
   @groups = current_user.groups
+  @message = Message.new
 end
 
 def create
-  @message = Message.
-
+  @groups = current_user.groups
+  @message = current_user.messages.new(message_params)
+  if @message.save
+    redirect_to group_messages_path
+  else
+    redirect_to group_messages_path,alert: "メッセージを入力してください。"
+  end
 end
 
 private
 def message_params
-
+  params.require(:message).permit(:body,:image).merge(group_id: params[:group_id])
 end
+  # def set_group
+  #   @group = Group.find(params[:group_id])
+  # end
 
+  # def set_messages
+  #   @messages = @group.messages
+  # end
 end
